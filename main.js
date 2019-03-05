77937301012883b214b645051172581e79853435
Brian <briansenpai14@gmail.com>
2019-03-05 21:25:19 +0200

    added a powerball feature to the application with some styles and the corresponding javascript

--- main.js
+++ main.js
@@ -1,27 +1,58 @@
-const generateButton = document.querySelector('#genNumber');
-const clearButton = document.querySelector('#clear');
-const displayNumbers = document.querySelector('main');
+const generateButton = document.querySelector("#genNumber");
+const generatePowerBall = document.querySelector("#genPowerBall");
+const clearNumbers = document.querySelector("#clearNumbers");
+const clearPower = document.querySelector("#clearPowerBall");
+const displayNumbers = document.querySelector("#display");
+const displayPowerBall = document.querySelector("#displayPower");
 let numberOfBalls = 0;
+let numberOfBall = 0;
 
-generateButton.addEventListener('click', function () {
+// running the functions after clicking the generate button
+generateButton.addEventListener("click", function() {
   generatingNumbers();
 });
 
-clearButton.addEventListener('click', function () {
-  displayNumbers.innerHTML = '';
-  numberOfBalls = 0;
-})
+generatePowerBall.addEventListener("click", function() {
+  generatingPowerBall();
+});
 
+// Clearing the random 5 numbers for powerball
+clearNumbers.addEventListener("click", function() {
+  displayNumbers.innerHTML = "";
+  numberOfBalls = 0;
+});
+
+// Clearing the powerball number
+clearPower.addEventListener("click", function() {
+  displayPowerBall.innerHTML = "";
+  numberOfBall = 0;
+});
+
+// Generating random 5 numbers for the powerball
 function generatingNumbers() {
-  while (numberOfBalls < 6) {
-    let randomNumber = Math.floor(Math.random() * 53)
-    let displayElement = document.createElement('div');
+  while (numberOfBalls < 5) {
+    let randomNumber = Math.floor(Math.random() * 51);
+    let displayElement = document.createElement("div");
     if (randomNumber === 0) {
-      randomNumber += 1; 
+      randomNumber += 1;
     }
-    displayElement.setAttribute('class','displayNumbers');
+    displayElement.setAttribute("class", "displayNumbers");
     displayElement.textContent = randomNumber;
     displayNumbers.appendChild(displayElement);
     numberOfBalls += 1;
+  }
+}
+// Generating the power ball number
+function generatingPowerBall() {
+  while (numberOfBall < 1) {
+    let randomNumber = Math.floor(Math.random() * 21);
+    let displayElement = document.createElement("div");
+    if (randomNumber === 0) {
+      randomNumber += 1;
+    }
+    displayElement.setAttribute("class", "displayNumbers");
+    displayElement.textContent = randomNumber;
+    displayPowerBall.appendChild(displayElement);
+    numberOfBall += 1;
   }
 }
